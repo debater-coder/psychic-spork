5d2ff731384f726b80e833fb70f4953b890f485e
debater-coder <52619668+debater-coder@users.noreply.github.com>
2023-10-23 21:17:52 +1100

    add idt

--- kernel/src/arch/x64/include.h
+++ kernel/src/arch/x64/include.h
@@ -1,3 +1,4 @@
 #pragma once
+#include "interrupts.h"
 
 void arch__exit();
--- kernel/src/main.c
+++ kernel/src/main.c
@@ -7,16 +7,35 @@
 #include "debug/include.h"
 #include "drivers/limine_framebuffer/include.h"
 
-typedef __attribute__((__packed__)) struct InterruptDescriptor64
+static inline void lidt(void *base, uint16_t size)
+{ // This function works in 32 and 64bit mode
+    struct
+    {
+        uint16_t length;
+        void *base;
+    } __attribute__((packed)) IDTR = {size, base};
+
+    asm("lidt %0" : : "m"(IDTR)); // let the compiler choose an addressing mode
+}
+
+static InterruptDescriptor64 idt[256];
+
+void handle_double_fault()
 {
-    uint16_t pointer_low;    // offset bits 0..15
-    uint16_t gdt_selector;   // a code segment selector in GDT or LDT
-    uint8_t ist;             // bits 0..2 holds Interrupt Stack Table offset, rest of bits zero.
-    uint8_t type_attributes; // gate type, dpl, and p fields
-    uint16_t pointer_middle; // offset bits 16..31
-    uint32_t pointer_high;   // offset bits 32..63
-    uint32_t reserved;       // reserved
-} InterruptDescriptor64;
+    panic("EXCEPTION_DOUBLE_FAULT");
+}
+
+void init_interrupts()
+{
+    uint16_t code_segment;
+
+    asm("mov %%cs, %0" : "=r"(code_segment));
+
+    uint16_t segment_selector = code_segment | 0x000; // GDT, privilege level 0
+
+    idt[8] = interrupts__new_entry(segment_selector, handle_double_fault, TRAP_GATE, 0);
+    lidt(idt, 255);
+}
 
 static volatile struct limine_framebuffer_request framebuffer_request = {
     .id = LIMINE_FRAMEBUFFER_REQUEST,
@@ -30,7 +49,7 @@ static volatile struct limine_memmap_request memmap_request = {
 // The following will be our kernel's entry point.
 // If renaming _start() to something else, make sure to change the
 // linker script accordingly.
-void _start(void)
+void _start()
 {
     // Ensure we got a framebuffer.
     if (framebuffer_request.response == NULL || framebuffer_request.response->framebuffer_count < 1)
@@ -44,28 +63,10 @@ void _start(void)
     TtyContext tty_ctx = drivers__limine_framebuffer__init(framebuffer);
     init_debug(&tty_ctx, &(TtyStyle){.bg = 0, .fg = 0xffffff});
 
-    if (memmap_request.response == NULL)
-    {
-        panic("No memory map!");
-    }
+    init_interrupts();
 
-    uint64_t usable_memory = 0;
-
-    printf("memory areas:\n");
-    for (uint64_t i = 0; i < memmap_request.response->entry_count; i++)
-    {
-        if ((*memmap_request.response->entries)[i].type == LIMINE_MEMMAP_USABLE)
-        {
-            printf("    start: 0x%x, length: 0x%x\n",
-                   (*memmap_request.response->entries)[i].base,
-                   (*memmap_request.response->entries)[i].length);
-
-            usable_memory += (*memmap_request.response->entries)[i].length;
-        }
-    }
-    printf("Usable memory: %x GiB\n", usable_memory / 1073741824ULL);
-
-    panic("PEBKAC");
+    int *ptr = (void *)0;
+    *ptr = 5;
 
     printf("It did not fail!\n");
 

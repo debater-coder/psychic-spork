c9fb831cf75af4189ac03862980ee0dfca2b72aa
debater-coder <52619668+debater-coder@users.noreply.github.com>
2023-10-24 19:47:10 +1100

    move the files

---
+++ kernel/src/interrupts.c
@@ -0,0 +1,60 @@
+#include <stdint.h>
+#include <arch/include.h>
+
+static InterruptDescriptor64 idt[256];
+
+void disable_pic()
+{
+    asm(".intel_syntax noprefix");
+    asm("mov al, 0xff; out 0xa1, al; out 0x21, al");
+    asm(".att_syntax prefix");
+}
+
+void init_interrupts()
+{
+    uint16_t code_segment;
+
+    asm("mov %%cs, %0" : "=r"(code_segment));
+
+    uint16_t segment_selector = code_segment | 0x000; // GDT, privilege level 0
+
+    idt[0] = interrupts__new_entry(segment_selector, handle_div_zero, TRAP_GATE, 0);
+    idt[3] = interrupts__new_entry(segment_selector, handle_breakpoint, TRAP_GATE, 0);
+    idt[8] = interrupts__new_entry(segment_selector, handle_double_fault, TRAP_GATE, 0);
+    idt[14] = interrupts__new_entry(segment_selector, handle_page_fault, TRAP_GATE, 0);
+
+    lidt(idt, 255);
+}
+
+static inline void lidt(void *base, uint16_t size)
+{ // This function works in 32 and 64bit mode
+    struct
+    {
+        uint16_t length;
+        void *base;
+    } __attribute__((packed)) IDTR = {size, base};
+
+    asm("lidt %0" : : "m"(IDTR)); // let the compiler choose an addressing mode
+}
+
+struct interrupt_frame;
+
+__attribute__((interrupt)) void handle_breakpoint(struct interrupt_frame *frame)
+{
+    printf("Breakpoint\n");
+}
+
+__attribute__((interrupt)) void handle_div_zero(struct interrupt_frame *frame)
+{
+    panic("EXCEPTION_DIV_ZERO");
+}
+
+__attribute__((interrupt)) void handle_page_fault(struct interrupt_frame *frame, uint64_t error_code)
+{
+    panic("EXCEPTION_PAGE_FAULT");
+}
+__attribute__((interrupt)) void
+handle_double_fault(struct interrupt_frame *frame, uint64_t error_code)
+{
+    panic("EXCEPTION_DOUBLE_FAULT");
+}
--- kernel/src/main.c
+++ kernel/src/main.c
@@ -7,51 +7,6 @@
 #include "debug/include.h"
 #include "drivers/limine_framebuffer/include.h"
 
-static inline void lidt(void *base, uint16_t size)
-{ // This function works in 32 and 64bit mode
-    struct
-    {
-        uint16_t length;
-        void *base;
-    } __attribute__((packed)) IDTR = {size, base};
-
-    asm("lidt %0" : : "m"(IDTR)); // let the compiler choose an addressing mode
-}
-
-static InterruptDescriptor64 idt[256];
-
-struct interrupt_frame;
-
-__attribute__((interrupt)) void handle_div_zero(struct interrupt_frame *frame)
-{
-    panic("EXCEPTION_DIV_ZERO");
-}
-
-__attribute__((interrupt)) void handle_page_fault(struct interrupt_frame *frame, uint64_t error_code)
-{
-    panic("EXCEPTION_PAGE_FAULT");
-}
-__attribute__((interrupt)) void
-handle_double_fault(struct interrupt_frame *frame, uint64_t error_code)
-{
-    panic("EXCEPTION_DOUBLE_FAULT");
-}
-
-void init_interrupts()
-{
-    uint16_t code_segment;
-
-    asm("mov %%cs, %0" : "=r"(code_segment));
-
-    uint16_t segment_selector = code_segment | 0x000; // GDT, privilege level 0
-
-    idt[0] = interrupts__new_entry(segment_selector, handle_div_zero, TRAP_GATE, 0);
-    idt[8] = interrupts__new_entry(segment_selector, handle_double_fault, TRAP_GATE, 0);
-    idt[14] = interrupts__new_entry(segment_selector, handle_page_fault, TRAP_GATE, 0);
-
-    lidt(idt, 255);
-}
-
 static volatile struct limine_framebuffer_request framebuffer_request = {
     .id = LIMINE_FRAMEBUFFER_REQUEST,
     .revision = 0};
@@ -87,10 +42,6 @@ void _start()
     init_debug(&tty_ctx, &(TtyStyle){.bg = 0, .fg = 0xffffff});
 
     init_interrupts();
-
-    divide_by_zero();
-    cause_page_fault();
-
     printf("It did not fail!\n");
 
     // We're done, just hang...
